ed66f6aad992215e15d62826b9e4366e27fdb31b
Sphereso <spheresox@gmail.com>
2024-01-25 10:51:31 +0100

    real init

--- db/migrations/20240122094947_create_user_table.up.sql
+++ db/migrations/20240122094947_create_user_table.up.sql
@@ -1,58 +1,102 @@
 
-CREATE TABLE KUNDE (
-    KUNDENNR        INTEGER NOT NULL,
-    NACHNAME        VARCHAR(50),
-    VORNAME         VARCHAR(50),
-    GEBURTSDATUM	DATE,
-	STRASSE         VARCHAR(50),
-	HAUSNR			VARCHAR(6),
-    PLZ             VARCHAR(5),
-    ORT             VARCHAR(50),
-    TELEFON         VARCHAR(25),
-    EMAIL           VARCHAR(50),
-    PRIMARY KEY (KUNDENNR)
+CREATE TABLE "Kunde" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Kunde_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE LIEFERANT (
-    LIEFERANTENNR   INTEGER NOT NULL,
-    LIEFERANTENNAME VARCHAR(50),
-    STRASSE         VARCHAR(50),
-    HAUSNR			VARCHAR(6),
-    PLZ             VARCHAR(5),
-    ORT             VARCHAR(50),
-    TELEFON			VARCHAR(25),
-    EMAIL           VARCHAR(50),
-    PRIMARY KEY (LIEFERANTENNR)
+CREATE TABLE "Bestellung" (
+    "id" SERIAL NOT NULL,
+    "kundeId" INTEGER NOT NULL,
+
+    CONSTRAINT "Bestellung_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE ZUTAT(
-    ZUTATENNR			INTEGER NOT NULL,
-    BEZEICHNUNG         VARCHAR(50),
-    EINHEIT        		VARCHAR (25),
-    NETTOPREIS       	DECIMAL(10,2),
-    BESTAND             INTEGER,
-    LIEFERANT			INTEGER,
-	KALORIEN			INTEGER,
-	KOHLENHYDRATE		DECIMAL (10,2),
-	PROTEIN			    DECIMAL(10,2),
-	PRIMARY KEY (ZUTATENNR),
-	FOREIGN KEY (LIEFERANT) REFERENCES LIEFERANT (LIEFERANTENNR)
+CREATE TABLE "Rezept" (
+    "id" SERIAL NOT NULL,
+    "bezeichnung" TEXT NOT NULL,
+    "beschreibung" TEXT NOT NULL,
+
+    CONSTRAINT "Rezept_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE BESTELLUNG (
-    BESTELLNR        SERIAL NOT NULL,
-    KUNDENNR         INTEGER,
-    BESTELLDATUM     DATE,
-    RECHNUNGSBETRAG  DECIMAL(10,2),
-    PRIMARY KEY (BESTELLNR),
-    FOREIGN KEY (KUNDENNR) REFERENCES KUNDE (KUNDENNR)
+CREATE TABLE "Ernaehrungskategorie" (
+    "id" SERIAL NOT NULL,
+    "bezeichnung" TEXT NOT NULL,
+
+    CONSTRAINT "Ernaehrungskategorie_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE BESTELLUNGZUTAT (
-    BESTELLNR       INTEGER NOT NULL,
-    ZUTATENNR       INTEGER,
-    MENGE     		INTEGER,
-    PRIMARY KEY (BESTELLNR,ZUTATENNR),
-    FOREIGN KEY (BESTELLNR) REFERENCES BESTELLUNG (BESTELLNR),
-    FOREIGN KEY (ZUTATENNR) REFERENCES ZUTAT (ZUTATENNR)
+CREATE TABLE "Allergene" (
+    "id" SERIAL NOT NULL,
+    "bezeichnung" TEXT NOT NULL,
+
+    CONSTRAINT "Allergene_pkey" PRIMARY KEY ("id")
 );
+
+CREATE TABLE "Zutat" (
+    "id" SERIAL NOT NULL,
+    "lieferantId" INTEGER NOT NULL,
+
+    CONSTRAINT "Zutat_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "ZutatenInBestellung" (
+    "zutatId" INTEGER NOT NULL,
+    "bestellungId" INTEGER NOT NULL,
+    "menge" INTEGER NOT NULL,
+
+    CONSTRAINT "ZutatenInBestellung_pkey" PRIMARY KEY ("zutatId","bestellungId")
+);
+
+CREATE TABLE "ZutatenInRezept" (
+    "zutatId" INTEGER NOT NULL,
+    "rezeptId" INTEGER NOT NULL,
+    "menge" INTEGER NOT NULL,
+
+    CONSTRAINT "ZutatenInRezept_pkey" PRIMARY KEY ("zutatId","rezeptId")
+);
+
+CREATE TABLE "Lieferant" (
+    "id" SERIAL NOT NULL,
+
+    CONSTRAINT "Lieferant_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "_ErnaehrungskategorieToRezept" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+CREATE TABLE "_AllergeneToRezept" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+CREATE UNIQUE INDEX "_ErnaehrungskategorieToRezept_AB_unique" ON "_ErnaehrungskategorieToRezept"("A", "B");
+
+CREATE INDEX "_ErnaehrungskategorieToRezept_B_index" ON "_ErnaehrungskategorieToRezept"("B");
+
+CREATE UNIQUE INDEX "_AllergeneToRezept_AB_unique" ON "_AllergeneToRezept"("A", "B");
+
+CREATE INDEX "_AllergeneToRezept_B_index" ON "_AllergeneToRezept"("B");
+
+ALTER TABLE "Bestellung" ADD CONSTRAINT "Bestellung_kundeId_fkey" FOREIGN KEY ("kundeId") REFERENCES "Kunde"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "Zutat" ADD CONSTRAINT "Zutat_lieferantId_fkey" FOREIGN KEY ("lieferantId") REFERENCES "Lieferant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "ZutatenInBestellung" ADD CONSTRAINT "ZutatenInBestellung_zutatId_fkey" FOREIGN KEY ("zutatId") REFERENCES "Zutat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "ZutatenInBestellung" ADD CONSTRAINT "ZutatenInBestellung_bestellungId_fkey" FOREIGN KEY ("bestellungId") REFERENCES "Bestellung"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "ZutatenInRezept" ADD CONSTRAINT "ZutatenInRezept_zutatId_fkey" FOREIGN KEY ("zutatId") REFERENCES "Zutat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "ZutatenInRezept" ADD CONSTRAINT "ZutatenInRezept_rezeptId_fkey" FOREIGN KEY ("rezeptId") REFERENCES "Rezept"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "_ErnaehrungskategorieToRezept" ADD CONSTRAINT "_ErnaehrungskategorieToRezept_A_fkey" FOREIGN KEY ("A") REFERENCES "Ernaehrungskategorie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "_ErnaehrungskategorieToRezept" ADD CONSTRAINT "_ErnaehrungskategorieToRezept_B_fkey" FOREIGN KEY ("B") REFERENCES "Rezept"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "_AllergeneToRezept" ADD CONSTRAINT "_AllergeneToRezept_A_fkey" FOREIGN KEY ("A") REFERENCES "Allergene"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "_AllergeneToRezept" ADD CONSTRAINT "_AllergeneToRezept_B_fkey" FOREIGN KEY ("B") REFERENCES "Rezept"("id") ON DELETE CASCADE ON UPDATE CASCADE;
