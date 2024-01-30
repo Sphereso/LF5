09fa922db1df906c41a887300791c07480f23006
Mika Bomm <mika.bomm@outlook.com>
2024-01-30 12:12:07 +0100

    Added Rezept to avoid getting errors Still doesn't work for migrations on my local pc

--- db/migrations/20240129095509_data.up.sql
+++ db/migrations/20240129095509_data.up.sql
@@ -34,11 +34,17 @@ INSERT INTO Adresse (ID, STRASSE, STADT, PLZ) VALUES (1501, 'Westerjork 74', 'Jo
 INSERT INTO Lieferant (ID, ADRESSEID, NAME, MAIL) VALUES (1501, 1501, 'Obst-Hof Altes Land', 'info@biohof-altesland.de');
 
 INSERT INTO Adresse (ID, STRASSE, STADT, PLZ) VALUES (1502, 'Molkereiwegkundekunde 13', 'Dechow', '19217');
-INSERT INTO Lieferant (ID, ADRESSEID, NAME, MAIL) VALUES (1502, 1502, 'Molkerei Henning', 'info@molkerei-henning.de')
+INSERT INTO Lieferant (ID, ADRESSEID, NAME, MAIL) VALUES (1502, 1502, 'Molkerei Henning', 'info@molkerei-henning.de');
+
+--REZEPT
+INSERT INTO Rezept(ID, bezeichnung, beschreibung) VALUES (5001, 'lachslasagne', 'Den Spinat 5 min vorgaren, auspressen bis er ganz trocken ist und würzen. Den Lachs etwas auftauen lassen, in kleine Stücke schneiden und mit Zitrone, Salz und Pfeffer würzen. Für die Bechamel-Soße wird die Butter im Topf erhitzt, das Mehl verrührt und mit der Milch aufgegossen. Die Brühe dazugeben und 5 min. kochen lassen. Danach die Sahne dazurühren und nochmals aufkochen lassen. Schließlich den Parmesan hinzufügen und mit Pfeffer und Muskat abschmecken. Jetzt alles abwechselnd in eine feuerfeste Form schichten, zuerst die Soße, dann Lasagneblätter, Soße, Spinat, Lachs, Käse, Lasagneblätter, usw. Letzte Schicht ist Soße. Die Lasagne anschließend bei 200 Grad 45 min. in den Backofen.');
+INSERT INTO Rezept(ID, bezeichnung, beschreibung) VALUES (5002, 'Thaicurry mit Hähnchen', 'Die Hühnerbrust in Streifen schneiden und in einer Marinade aus Sojasauce, Pfeffer und Zucker ca. 1 Stunde marinieren. Im Wok oder in der Pfanne gut anbraten. Herausnehmen. Gemischtes Gemüse und Bambussprossen waschen und sehr fein schneiden, ebenfalls anbraten, dann herausnehmen. Die Currypaste und die Kokosmilch im Wok verrühren und kurz (!) durchköcheln lassen. Dann Fleisch und Gemüse wieder zugeben und noch kurz köcheln lassen. Mit Sojasauce abschmecken. In der Zwischenzeit den Reis kochen und abtropfen lassen.');
+INSERT INTO Rezept(ID, bezeichnung, beschreibung) VALUES (5003, 'Kartoffelsuppe', 'Kartoffeln und Karotte schälen und in grobe Würfel schneiden. Zwiebel würfeln. Lauch in Ringe schneiden. Zwiebeln in etwas Olivenöl glasig dünsten, Lauch dazugeben. Mit der Gemüsebrühe ablöschen. Die gewürfelten Kartoffeln und Karotte in die Brühe geben. Alles für ca. 20 min köcheln lassen. Anschließend alles mit dem Pürierstab zu einer Suppe pürieren, mit Pfeffer und Muskat abschmecken. Die in Scheiben geschnittenen Würstchen in den Suppenteller geben und die heiße Kartoffelsuppe darüber gießen.');
+
 
 --BESTELLUNGEN
 INSERT INTO Bestellung (ID, kundeId, bestellungDatum) VALUES (1001, 1001,'2020-07-01');
-INSERT INTO RezepteInBestellung(rezeptId, bestellungId, menge) VALUES (5002, 1001, 50);
+INSERT INTO RezepteInBestellung(rezeptId, bestellungId, menge) VALUES (5001, 1001, 50);
 
 INSERT INTO Bestellung (ID, kundeId, bestellungDatum) VALUES (1002, 1002,'2020-07-08');
 INSERT INTO RezepteInBestellung(rezeptId, bestellungId, menge) VALUES (5001, 1002, 1);
@@ -46,7 +52,6 @@ INSERT INTO RezepteInBestellung(rezeptId, bestellungId, menge) VALUES (5001, 100
 
 INSERT INTO Bestellung (ID, kundeId, bestellungDatum) VALUES (1003, 1003,'2020-08-01');
 INSERT INTO RezepteInBestellung(rezeptId, bestellungId, menge) VALUES (5003, 1003, 3);
-
 
 INSERT INTO Bestellung (ID, kundeId, bestellungDatum) VALUES (1004, 1004,'2020-08-02');
 INSERT INTO RezepteInBestellung(rezeptId, bestellungId, menge) VALUES (5002, 1004, 5);
