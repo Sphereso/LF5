9a7ac42291c645f3f27dab94e53d1817e281eb3b
Mika <mika.bomm@outlook.com>
2024-02-05 18:35:58 +0100

    updated 1. stored procedure to be dynamic

--- db/migrations/20240129095509_data.up.sql
+++ db/migrations/20240129095509_data.up.sql
@@ -184,16 +184,15 @@ INSERT INTO ernaehrungskategorieToRezept (ernaehrungskategorieId, rezeptId) VALU
 --Stored procedures
 --Procedure to get all lieferanten in a city
 
-CREATE OR REPLACE PROCEDURE get_lieferant()
+CREATE OR REPLACE PROCEDURE get_lieferant(p_stadt VARCHAR)
 LANGUAGE plpgsql
 AS $$
 DECLARE
     rec RECORD;
 BEGIN
     FOR rec IN SELECT lieferant.name, lieferant.mail, adresse.strasse, adresse.stadt, adresse.plz 
-               FROM lieferant JOIN adresse ON lieferant.adresseid = adresse.id WHERE adresse.stadt = 'Jork' LOOP
+               FROM lieferant JOIN adresse ON lieferant.adresseid = adresse.id WHERE adresse.stadt = p_stadt LOOP
         RAISE NOTICE 'Name: %, Mail: %, Street: %, City: %, Zip: %', rec.name, rec.mail, rec.strasse, rec.stadt, rec.plz;
     END LOOP;
 END
 $$;
-
