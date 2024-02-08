04441702612ba674f698d942311d633d350b3717
Mika Bomm <mika.bomm@outlook.com>
2024-02-08 12:14:49 +0100

    Add delete_user and update_user_id stored procedures

--- db/migrations/20240129095509_data.up.sql
+++ db/migrations/20240129095509_data.up.sql
@@ -195,3 +195,44 @@ BEGIN
     END LOOP;
 END
 $$;
+
+CREATE PROCEDURE delete_user(user_id UUID)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+    DELETE FROM rezepteinbestellung
+    WHERE rezepteinbestellung.bestellungId IN (
+        SELECT id
+        FROM bestellung
+        WHERE bestellung.kundeId = user_id
+    );
+
+    DELETE FROM bestellung
+    WHERE bestellung.kundeId = user_id;
+    
+    DELETE FROM adresse
+    WHERE adresse.id IN (
+        SELECT id
+        FROM kunde
+        WHERE kunde.id = user_id
+    );
+     
+    DELETE FROM kunde
+    WHERE kunde.id = user_id;
+    
+    RAISE NOTICE 'User data deleted successfully.';
+END
+$$;
+
+CREATE PROCEDURE update_user_id(old_user_id UUID, new_user_name VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE kunde
+    SET name = new_user_name
+    WHERE id = old_user_id;
+    
+    RAISE NOTICE 'User Name updated successfully.';
+END
+$$;
